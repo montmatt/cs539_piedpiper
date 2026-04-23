01d9e932ff7eb15645eb40eea5ca9c24107d31ed
BaretBuckley <thejocky10000@gmail.com>
2026-04-23 17:44:53 -0400

    finished affect vec implimentation

---
+++ src/LinkedMatrix.zig
@@ -0,0 +1,72 @@
+const std = @import("std");
+
+const LinkedMatrix = @This();
+
+const Link = struct {
+    data: [*]f32, // Explicitly not labeled length
+    next: ?*Link,
+};
+
+head: ?*Link,
+tail: ?*Link,
+len: usize,
+width: usize,
+
+pub fn init(width: usize) LinkedMatrix {
+    return .{
+        .head = null,
+        .tail = null,
+        .len = 0,
+        .width = width,
+    };
+}
+
+// Copys the given row (from) and appends to the end of the list
+pub fn appendFrom(self: *LinkedMatrix, alloc: std.mem.Allocator, from: []f32) !void {
+    const link = try alloc.create(Link);
+    const ownedRow = try alloc.alloc(f32, from.len);
+    if (from.len != self.width) unreachable;
+    self.len += 1;
+    std.mem.copyForwards(f32, ownedRow, from);
+    link.* = .{
+        .data = ownedRow.ptr,
+        .next = null,
+    };
+
+    if (self.tail == null) {
+        self.head = link;
+        self.tail = link;
+    } else {
+        self.tail.?.next = link;
+        self.tail = link;
+    }
+}
+
+const Iter = struct {
+    cur: ?*Link,
+    width: usize,
+
+    pub fn next(self: *Iter) ?[]f32 {
+        if (self.cur == null) return null;
+        const res: []f32 = self.cur.?.data[0..self.width]; // self.width
+        self.cur = self.cur.?.next;
+        return res;
+    }
+};
+
+pub fn iter(self: *LinkedMatrix) Iter {
+    return .{
+        .cur = self.head,
+        .width = self.width,
+    };
+}
+
+pub fn get(self: *LinkedMatrix, idx: usize) ![]f32 {
+    var curIdx: usize = 0;
+    var curIter = self.iter();
+    while (curIter.next()) |val| {
+        if (curIdx == idx) return val[0..self.width];
+        curIdx += 1;
+    }
+    return error.IndexOutOfRange;
+}
