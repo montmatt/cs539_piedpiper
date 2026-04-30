e2b17fcb6e2b58b0e439f8974795cc3cfb73dfc5
2Carrot10 <eleanorgrutz@gmail.com>
2026-04-30 05:18:39 -0400

    Add most new functions to exported values

--- src/main.zig
+++ src/main.zig
@@ -2,52 +2,73 @@ const std = @import("std");
 
 const GenericVec = @import("UnionedVec.zig");
 
-var avec: GenericVec = undefined;
+var vec: GenericVec = undefined;
 
-export fn init(anyVec: [*:0]const u8, numClusters: u32) bool {
-    avec = .new();
-    std.debug.print("Recieved string: {s}\n", .{std.mem.span(anyVec)});
-    if (!avec.init(std.mem.span(anyVec))) {
+export fn init(aVec: [*:0]const u8, wVec: [*:0]const u8, numClusters: u32) bool {
+    vec = .new();
+    if (!vec.init(std.mem.span(aVec), std.mem.span(wVec))) {
         return false;
     }
-    std.debug.print("Labels:\n", .{});
-    for (avec.labels.items) |label| {
+    std.debug.print("Labels (word vec):\n", .{});
+    for (vec.labels_word.items) |label| {
         std.debug.print("{s} ", .{label});
     }
+
+    std.debug.print("Labels (affect vec):\n", .{});
+    for (vec.labels_affect.items) |label| {
+        std.debug.print("{s} ", .{label});
+    }
+
     std.debug.print("\n", .{});
 
-    const sum = avec.sumAbsValues(avec.arena.allocator()) catch |err| {
+    const sum = vec.sumAbsValuesAffect(vec.arena.allocator()) catch |err| {
         std.debug.print("Failed to sum values. Error: {}\n", .{err});
         return true;
     };
-    std.debug.print("The sum is {any}", .{sum});
+    std.debug.print("The sum of affect vecs is {any}", .{sum});
 
     _ = numClusters;
     return true;
 }
 
 export fn deinit() void {
-    avec.deinit();
+    vec.deinit();
 }
 
 export fn numDataPoints() usize {
-    // return avec.
+    // return vec.
     return 0;
 }
 
-export fn numAffectVecCols() usize {
-    return avec.labels.items.len;
+export fn numAffectVecColsAffect() usize {
+    return vec.labels_affect.items.len;
 }
 
-export fn numAffectVecRows() usize {
-    return avec.values.len;
+export fn numAffectVecColsWord() usize {
+    return vec.labels_word.items.len;
 }
 
-export fn numWordVecCols() usize {
+export fn numAffectVecRowsAffect() usize {
+    return vec.values_affect.len;
+}
+
+export fn numAffectVecRowsWord() usize {
+    return vec.values_word.len;
+}
+
+export fn numWordVecColsAffect() usize {
     return 0;
 }
 
-export fn numWordVecRows() usize {
+export fn numWordVecColsWord() usize {
+    return 0;
+}
+
+export fn numWordVecRowsAffect() usize {
+    return 0;
+}
+
+export fn numWordVecRowsWord() usize {
     return 0;
 }
 
@@ -60,7 +81,7 @@ export fn getWordVecvalue(row: usize, axis: usize) f32 {
 
 // Returns position of word in AffectVec database on given axis
 export fn getAffectVecValue(row: usize, axis: usize) f32 {
-    return avec.get(row)[axis] catch |err| {
+    return vec.get(row)[axis] catch |err| {
         std.debug.print("Failed to read affectVecValue {}", .{err});
         return null;
     };
@@ -69,7 +90,7 @@ export fn getAffectVecValue(row: usize, axis: usize) f32 {
 // Returns vector of the given word in the WordVec database
 // Length of vector is size of numWordVecRows()
 export fn getWordVecPos(row: usize) ?[*]f32 {
-    // return avec.get(row).ptr;
+    // return vec.get(row).ptr;
     return null;
 }
 
